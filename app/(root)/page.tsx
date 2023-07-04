078858f7c6f160e5ce4d1a54d68e9dea61757c6d
md-amir-gauhar <gauharamir15@gmail.com>
2023-07-05 00:37:28 +0530

    setup authentication using clerk/nextjs

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function SetupPage() {
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
